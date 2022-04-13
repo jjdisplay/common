5c1d95095b8d64f72e5856391c2e7683f936dafb
james <james.ee.developer@gmail.com>
2022-04-13 18:38:22 +0800

    revised download.sh

--- download.sh
+++ download.sh
@@ -1,4 +1,4 @@
 mkdir -p testdir \
 && wget https://github.com/jjdisplay/common/raw/main/serviceAccountKey-latest.zip -O testdir/temp.zip \
-&& cd testdir &&  unzip -P jjdisplay1802 -o temp.zip && rm temp.zip && cd ..
+&& cd testdir &&  unzip -P $1 -o temp.zip && rm temp.zip && cd ..
 
