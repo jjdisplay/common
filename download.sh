0fbc58000ab501b39c989a03279eb86b135205df
james <james.ee.developer@gmail.com>
2022-04-13 19:04:12 +0800

    feat: download.sh

--- download.sh
+++ download.sh
@@ -1,5 +1,5 @@
 # wget https://raw.githubusercontent.com/jjdisplay/common/main/download.sh && bash download.sh <password>
 DIR=.config/firebase
-
-wget https://github.com/jjdisplay/common/raw/main/serviceAccountKey-latest.zip -O temp.zip \
-&& unzip -P $1 -o temp.zip && mv serviceAccountKey.json.encrypted ${DIR}/ && rm temp.zip download.sh && cd
+FILENAME=serviceAccountKey-latest.zip
+wget https://github.com/jjdisplay/common/raw/main/${FILENAME} \
+&& unzip -P $1 -o ${FILENAME} && mv serviceAccountKey.json.encrypted ${DIR}/ && rm ${FILENAME} download.sh
